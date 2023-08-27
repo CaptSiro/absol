c07fa37b59f3df226ed078d53dc6879005ff0bb8
CaptSiro <honzahorak433@icloud.com>
2023-08-27 13:53:54 +0200

    Implemented init

---
+++ src/init.php
@@ -0,0 +1,16 @@
+<?php
+
+function init($cwd) {
+    if (file_exists("$cwd/absol.lock.json")) {
+        echo "absol has been already initialized";
+    }
+
+    if (!file_exists("$cwd/absol")) {
+        mkdir("$cwd/absol");
+    }
+
+    copy(__DIR__ . "/../dist/import.php", "$cwd/absol/import.php");
+    touch("$cwd/absol/packages.json");
+    copy(__DIR__ . "/../dist/absol.json", "$cwd/absol.json");
+    copy(__DIR__ . "/../dist/absol.json", "$cwd/absol.lock.json");
+}
