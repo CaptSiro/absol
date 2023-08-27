64867aa9a9a268e6b6f5e0e8175f45dad2329250
CaptSiro <honzahorak433@icloud.com>
2023-08-27 15:18:54 +0200

    Implemented import.php

--- dist/import.php
+++ dist/import.php
@@ -1,5 +1,17 @@
 <?php
 
-function import($package, $module) {
+function import($package, $module = "") {
+    if ($module === "") {
+        $module = "index";
+    }
 
+    if (!file_exists(__DIR__ . "/$package")) {
+        throw new Exception("Package '$package' does not exists");
+    }
+
+    if (!file_exists(__DIR__ . "/$package/absol_modules/$module.php")) {
+        throw new Exception("Package '$package' does not include ". ($module === "index" ? "default" : $module) ." absol module");
+    }
+
+    require_once __DIR__ . "/$package/absol_modules/$module.php";
 }
