c44581dfcaac9995fc410152eceef22fcef91e5f
CaptSiro <honzahorak433@icloud.com>
2023-08-29 17:03:21 +0200

    Updated pickup command to pickup project dependencies

--- src/commands/pickup.php
+++ src/commands/pickup.php
@@ -4,17 +4,20 @@ global $cwd;
 
 require_once __DIR__ . "/../package-name.php";
 require_once __DIR__ . "/../rmdir-recursive.php";
-require_once __DIR__ . "/../mktmp.php";
+require_once __DIR__ . "/../dep/add.php";
+require_once __DIR__ . "/../generate-lock-file.php";
 
-function pickup($cwd, $git_repo) {
-    $name = package_name($git_repo);
+function pickup($cwd, $git_repo, $add_to_dep = true) {
+    $package = package_name($git_repo);
 
-    $path = "$cwd/absol/$name";
+    $path = "$cwd/absol/$package";
 
-    $git_dir = mktmp($cwd);
-    echo $git_dir;
+    if (file_exists($path)) {
+        echo "Skipped ". package_name_pretty($package) ." because it has been already picked up.\n";
+        return;
+    }
 
-    shell_exec("git clone --separate-git-dir=$git_dir --depth=1 $git_repo $path");
+    shell_exec("git clone -q --depth=1 $git_repo $path");
 
     if (!file_exists($path)) {
         echo "\nError while cloning git repository";
@@ -22,18 +25,48 @@ function pickup($cwd, $git_repo) {
     }
 
     if (!file_exists("$path/absol.json")) {
-        echo "Invalid package: '$name' does not include file: 'absol.json'\n";
+        echo "Invalid package: '$package' does not include file: 'absol.json'\n";
         rmdir_recursive($path);
         return;
     }
 
     if (!file_exists("$path/absol_modules/index.php")) {
-        echo "Invalid package: '$name' does not include default import file: 'absol_modules/index.php'\n";
+        echo "Invalid package: '$package' does not include default import file: 'absol_modules/index.php'\n";
         rmdir_recursive($path);
         return;
     }
 
-    echo "\nDo not forget to clean $cwd/absol/tmp folder\nI would do it but I'm too cool 😎 to do your work";
+    if (!file_exists("$path/absol")) {
+        mkdir("$path/absol");
+    }
+
+    if (file_exists("$path/absol/import.php")) {
+        unlink("$path/absol/import.php");
+    }
+
+    link(__DIR__ . "/../../dist/library-import.php", "$path/absol/import.php");
+
+    if ($add_to_dep) {
+        dep_add("$cwd/absol.json", $git_repo);
+    }
+
+    pickup_deps($cwd, "$path/absol.json");
+
+    echo package_name_pretty($package) ." has been picked up. It is rather heavy.\n";
 }
 
-pickup($cwd, $argv[0]);
+function pickup_deps($cwd, $file) {
+    $json = json_decode(file_get_contents($file));
+
+    if (isset($json->dependencies)) {
+        foreach ($json->dependencies as $dependency) {
+            pickup($cwd, $dependency, false);
+        }
+    }
+}
+
+if (isset($argv[0])) {
+    pickup($cwd, $argv[0]);
+} else {
+    pickup_deps($cwd, "$cwd/absol.php");
+}
